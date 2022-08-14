db98f05752cf4aac1b54606581d98e7d548b65d9
Christopher <c.bickel1@web.de>
2022-08-14 22:18:20 +0200

    Improved performance

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,21 @@ mod systems;
 
 use bevy::{
     app::App,
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     hierarchy::BuildChildren,
     math::Vec3,
     prelude::{
         default, shape::RegularPolygon, AssetServer, Assets, Camera2dBundle, Color, Commands, Mesh,
         Msaa, Res, ResMut, Transform,
     },
-    reflect::TypeUuid,
-    render::render_resource::{AsBindGroup, ShaderRef},
-    sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle},
+    sprite::{ColorMaterial, ColorMesh2dBundle},
     window::WindowDescriptor,
     DefaultPlugins,
 };
 
 use bevy_easings::EasingsPlugin;
 use bevy_inspector_egui::{RegisterInspectable, WorldInspectorPlugin};
-use chrono::Utc;
+// use chrono::Utc;
 use components::{Cell, HiddenCell, HoveredCell, MainCamera};
 use interactable::{hover::Hoverable, shapes::*};
 use rand::{thread_rng, Rng};
@@ -41,8 +40,9 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(EasingsPlugin)
-        .add_plugin(Material2dPlugin::<CustomMaterial>::default())
         .add_plugin(WorldInspectorPlugin::new())
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup)
         .add_system(helpers::camera::movement)
         .add_system(helpers::texture::set_texture_filters_to_nearest)
@@ -57,8 +57,8 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<CustomMaterial>>,
-    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    // asset_server: Res<AssetServer>,
 ) {
     commands
         .spawn_bundle(Camera2dBundle::default())
@@ -66,19 +66,50 @@ fn setup(
 
     let mut rng = thread_rng();
 
-    let big_hexagon = RegularPolygon::new(RADIUS, 6);
-    let medium_hexagon = RegularPolygon::new(RADIUS * 0.94, 6);
-    let small_hexagon = RegularPolygon::new(RADIUS * 0.76, 6);
+    let medium_transform = Transform::from_translation(Vec3::new(0.0, 0.0, 3.0));
+    let small_transform = Transform::from_translation(Vec3::new(0.0, 0.0, 4.0));
 
-    let yellow = (Color::hex("d4aa00").unwrap(), Color::hex("ffcc00").unwrap());
-    let blue = (Color::hex("0088aa").unwrap(), Color::hex("00aad4").unwrap());
-    let gray = (Color::hex("24221c").unwrap(), Color::hex("484537").unwrap());
+    let big_hexagon = meshes.add(Mesh::from(RegularPolygon::new(RADIUS, 6)));
+    let medium_hexagon = meshes.add(Mesh::from(RegularPolygon::new(RADIUS * 0.94, 6)));
+    let small_hexagon = meshes.add(Mesh::from(RegularPolygon::new(RADIUS * 0.76, 6)));
 
-    let medium_transform = Transform::from_translation(Vec3::new(0.0, 0.0, 2.0));
-    let small_transform = Transform::from_translation(Vec3::new(0.0, 0.0, 3.0));
+    let white = materials.add(ColorMaterial {
+        color: Color::WHITE,
+        ..default()
+    });
+    let yellow = (
+        materials.add(ColorMaterial {
+            color: Color::hex("d4aa00").unwrap(),
+            ..default()
+        }),
+        materials.add(ColorMaterial {
+            color: Color::hex("ffcc00").unwrap(),
+            ..default()
+        }),
+    );
+    let blue = (
+        materials.add(ColorMaterial {
+            color: Color::hex("0088aa").unwrap(),
+            ..default()
+        }),
+        materials.add(ColorMaterial {
+            color: Color::hex("00aad4").unwrap(),
+            ..default()
+        }),
+    );
+    let gray = (
+        materials.add(ColorMaterial {
+            color: Color::hex("24221c").unwrap(),
+            ..default()
+        }),
+        materials.add(ColorMaterial {
+            color: Color::hex("484537").unwrap(),
+            ..default()
+        }),
+    );
 
-    for x in 0..4 {
-        for y in 0..4 {
+    for x in 0..50 {
+        for y in 0..50 {
             let tx = (x * 45) as f32 + RADIUS;
             let ty = (y + 1) as f32 * RADIUS * 2.0
                 - match x % 2 {
@@ -87,34 +118,32 @@ fn setup(
                 };
             let rand = rng.gen_range(0..3);
             let colors = match rand {
-                0 => yellow,
-                1 => blue,
-                _ => gray,
+                0 => yellow.clone(),
+                1 => blue.clone(),
+                _ => gray.clone(),
             };
 
-            let mut big_transform = Transform::from_translation(Vec3::new(tx, ty, 1.0));
+            let mut big_transform = Transform::from_translation(Vec3::new(tx, ty, 2.0));
             big_transform.rotate_z(f32::to_radians(90.0));
             let cell = commands
                 .spawn()
-                .insert_bundle(MaterialMesh2dBundle {
-                    mesh: bevy::sprite::Mesh2dHandle(meshes.add(Mesh::from(big_hexagon))),
+                .insert_bundle(ColorMesh2dBundle {
+                    mesh: big_hexagon.clone().into(),
+                    material: white.clone().into(),
                     transform: big_transform,
-                    material: materials.add(CustomMaterial {
-                        color: Color::WHITE,
-                    }),
                     ..default()
                 })
                 .with_children(|parent| {
-                    parent.spawn_bundle(MaterialMesh2dBundle {
-                        mesh: bevy::sprite::Mesh2dHandle(meshes.add(Mesh::from(medium_hexagon))),
+                    parent.spawn_bundle(ColorMesh2dBundle {
+                        mesh: medium_hexagon.clone().into(),
                         transform: medium_transform,
-                        material: materials.add(CustomMaterial { color: colors.0 }),
+                        material: colors.0.into(),
                         ..default()
                     });
-                    parent.spawn_bundle(MaterialMesh2dBundle {
-                        mesh: bevy::sprite::Mesh2dHandle(meshes.add(Mesh::from(small_hexagon))),
+                    parent.spawn_bundle(ColorMesh2dBundle {
+                        mesh: small_hexagon.clone().into(),
                         transform: small_transform,
-                        material: materials.add(CustomMaterial { color: colors.1 }),
+                        material: colors.1.into(),
                         ..default()
                     });
                 })
@@ -136,18 +165,5 @@ fn setup(
                 commands.entity(cell).insert(HiddenCell);
             }
         }
-    }
-}
-
-#[derive(AsBindGroup, TypeUuid, Debug, Clone)]
-#[uuid = "d0165c41-d5c1-47cb-961c-335f1ab8b274"]
-struct CustomMaterial {
-    #[uniform(0)]
-    color: Color,
-}
-
-impl Material2d for CustomMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "shaders/custom_material.wgsl".into()
     }
 }
