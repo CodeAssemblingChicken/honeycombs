42ebe56bec2cc3cf00172a54c824296cbee5210b
Christopher <c.bickel1@web.de>
2022-09-13 21:40:15 +0200

    Fixing release #2

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ extern crate embed_resource;
 fn main() {
     let target = std::env::var("TARGET").unwrap();
     if target.contains("windows") {
+        static_vcruntime::metabuild();
         embed_resource::compile("icon.rc");
     }
 }
